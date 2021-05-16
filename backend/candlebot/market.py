f222ce82afdfdaa84a7a3fd3e99da5eef8be6e15
Oriol Alfonso Riba <oriolalfonso91@gmail.com>
2021-05-16 19:17:18 +0200

    Calculate percentages + use only tickets

--- backend/candlebot/market.py
+++ backend/candlebot/market.py
@@ -7,41 +7,31 @@ logger = logging.getLogger(__name__)
 
 class Binance:
     @staticmethod
-    def parse_price(response, symbol=None):
+    def parse_prices(response, symbol=None):
+        market_values = {}
         response_data = response.json()
-        return response_data['price']
-
-    @staticmethod
-    def parse_symbols(response, symbol=None):
-        response_data = response.json()
-        symbols = [
-            symbol['symbol']
-            for symbol in response_data['symbols'][:20]
-            if 'USDT' in symbol['symbol']
-        ]
-        return symbols
+        for data in response_data:
+            symbol = data['symbol']
+            if 'USDT' in symbol:
+                market_values[symbol] = data['price']
+        return market_values
 
 
 class Crypto:
     @staticmethod
-    def parse_price(response, symbol=None):
+    def parse_prices(response, symbol=None):
+        market_values = {}
         response_data = response.json()
-        return response_data['result']['data'][-1]['c']
-
-    @staticmethod
-    def parse_symbols(response, symbol=None):
-        response_data = response.json()
-        symbols = [
-            symbol['instrument_name']
-            for symbol in response_data['result']['instruments'][:3]
-            if 'USDT' in symbol['instrument_name']
-        ]
-        return symbols
+        for data in response_data['result']['data']:
+            symbol = data['i']
+            if 'USDT' in symbol:
+                market_values[symbol] = data['l']
+        return market_values
 
 
 class Poloniex:
     @staticmethod
-    def parse_price(response, symbol=None):
+    def parse_prices(response, symbol=None):
         market_values = {}
         response_data = response.json()
         for symbol, data in response_data.items():
@@ -52,11 +42,23 @@ class Poloniex:
 
 class Wazirx:
     @staticmethod
-    def parse_price(response, symbol=None):
+    def parse_prices(response, symbol=None):
         market_values = {}
         response_data = response.json()
         for symbol, data in response_data.items():
             if 'usdt' in symbol:
+                market_values[symbol] = data['last']
+        return market_values
+
+
+class Gateio:
+    @staticmethod
+    def parse_prices(response, symbol=None):
+        market_values = {}
+        response_data = response.json()
+        for data in response_data:
+            symbol = data['currency_pair']
+            if 'USDT' in symbol:
                 market_values[symbol] = data['last']
         return market_values
 
@@ -67,35 +69,28 @@ class Market:
     CRYPTO = 'crypto'
     POLONIEX = 'poloniex'
     WAZIRX = 'wazirx'
+    GATEIO = 'gateio'
 
     APIS = {
         BINANCE: {
-            'price_endpoint': 'https://api3.binance.com/api/v3/avgPrice?symbol={symbol}',  # noqa
-            'price_parser': Binance.parse_price,
-            'symbols_endpoint': 'https://api3.binance.com/api/v3/exchangeInfo',
-            'symbols_parser': Binance.parse_symbols,
-            'symbols': [],
+            'prices_endpoint': 'https://api3.binance.com/api/v3/ticker/price',
+            'prices_parser': Binance.parse_prices,
         },
         CRYPTO: {
-            'price_endpoint': 'https://api.crypto.com/v2/public/get-candlestick?instrument_name={symbol}&timeframe=1m',  # noqa
-            'price_parser': Crypto.parse_price,
-            'symbols_endpoint': 'https://api.crypto.com/v2/public/get-instruments',  # noqa
-            'symbols_parser': Crypto.parse_symbols,
-            'symbols': [],
+            'prices_endpoint': 'https://api.crypto.com/v2/public/get-ticker',
+            'prices_parser': Crypto.parse_prices,
         },
         POLONIEX: {
-            'price_endpoint': 'https://poloniex.com/public?command=returnTicker',  # noqa
-            'price_parser': Poloniex.parse_price,
-            'symbols_endpoint': '',
-            'symbols_parser': None,
-            'symbols': [],
+            'prices_endpoint': 'https://poloniex.com/public?command=returnTicker',  # noqa
+            'prices_parser': Poloniex.parse_prices,
         },
         WAZIRX: {
-            'price_endpoint': 'https://api.wazirx.com/api/v2/tickers',  # noqa
-            'price_parser': Wazirx.parse_price,
-            'symbols_endpoint': '',
-            'symbols_parser': None,
-            'symbols': [],
+            'prices_endpoint': 'https://api.wazirx.com/api/v2/tickers',
+            'prices_parser': Wazirx.parse_prices,
+        },
+        GATEIO: {
+            'prices_endpoint': 'https://api.gateio.ws/api/v4/spot/tickers',
+            'prices_parser': Gateio.parse_prices,
         },
     }
 
@@ -105,37 +100,15 @@ class Market:
         market_status = defaultdict(dict)
         for name, data in cls.APIS.items():
             logger.info(f'CEX: {name}')
-            if data['symbols_endpoint']:
-                cls._fill_symbols(data)
-                for symbol in data['symbols']:
-                    uri = data['price_endpoint'].format(symbol=symbol)
-                    market_value = cls._request_market_value(
-                        uri, data['price_parser'], name, symbol)
-                    market_status[name][symbol] = market_value
-            else:  # poloniex endpoint returns data for all symbols
-                response = requests.get(data['price_endpoint'])
-                try:
-                    market_values = data['price_parser'](response)
-                except Exception:
-                    logging.exception(f'Request error: {name}, {symbol}')
-                market_status[name] = market_values
+            response = requests.get(data['prices_endpoint'])
+            try:
+                market_values = data['prices_parser'](response)
+            except Exception:
+                logging.exception(f'Request error: {name}')
+            market_status[name] = market_values
         reconciled_prices = cls._reconcile_prices(market_status)
         compared_prices = cls._compare_reconciled_prices(reconciled_prices)
         return compared_prices
-
-    @classmethod
-    def _fill_symbols(cls, data):
-        symbols_response = requests.get(data['symbols_endpoint'])
-        data['symbols'] = data['symbols_parser'](symbols_response)
-
-    @classmethod
-    def _request_market_value(cls, uri, parser, name, symbol=None):
-        response = requests.get(uri)
-        try:
-            market_value = parser(response, symbol)
-        except Exception:
-            logging.exception(f'Request error: {name}, {symbol}')
-        return market_value
 
     @classmethod
     def _reconcile_prices(cls, market_status):
@@ -181,11 +154,17 @@ class Market:
                     'low': min_price,
                     'high': max_price,
                 }
-                compared_price['best_percent'] = (
-                    (float(max_price['price']) - float(min_price['price']))
-                    / float(min_price['price'])
-                    * 100
-                )
+                if float(min_price['price']):
+                    compared_price['best_percent'] = (
+                        (float(max_price['price']) - float(min_price['price']))
+                        / float(min_price['price'])
+                        * 100
+                    )
+                else:
+                    logging.error(
+                        f'{symbol}: max: {max_price} min: {min_price}. '
+                        'Min price is 0.'
+                    )
                 compared_prices.append(compared_price)
         return sorted(
             compared_prices,
