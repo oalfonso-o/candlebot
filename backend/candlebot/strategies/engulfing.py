d55c7c1b73fc084e6b3496d13b71c1ef437a1c68
Oriol Alfonso Riba <oriolalfonso91@gmail.com>
2021-05-25 11:44:25 +0200

    Add direction for engulfing

--- backend/candlebot/strategies/engulfing.py
+++ backend/candlebot/strategies/engulfing.py
@@ -18,25 +18,46 @@ class StrategyEngulfing:
     def __init__(self, df: pd.DataFrame):
         self.df = df
         self.wallet = Wallet()
+        self.last_open_candle = 0
         for indicator in self.indicators:
             self.df = indicator.apply(self.df)
 
     def calc(self) -> Tuple[pd.DataFrame, dict]:
+        direction = 0
         for i, row in self.df.iterrows():
-            if self._must_open_long(row):
+            if self._must_open_long(row, direction):
                 timestamp = utils.datetime_to_timestamp(
                     row['_id'].to_pydatetime()
                 )
                 self.wallet.open_pos('long', row['close'], timestamp)
-            elif self._must_close_long(row):
+                self.last_open_candle = row['open']
+                direction = 1
+            elif self._must_close_long(row, direction):
+                close = row['close']
                 timestamp = utils.datetime_to_timestamp(
                     row['_id'].to_pydatetime()
                 )
-                self.wallet.close_pos('long', row['close'], timestamp)
+                if row['close'] < self.last_open_candle:
+                    close = self.last_open_candle
+                self.wallet.close_pos('long', close, timestamp)
+                direction = -1
         return self.df, self.wallet
 
-    def _must_open_long(self, row):
+    def _must_open_long(self, row, direction):
+        if (
+            (not direction or direction == -1)
+            and row['engulfing'] == 100
+        ):
+            return True
         return False
 
-    def _must_close_long(self, row):
+    def _must_close_long(self, row, direction):
+        if (
+            (not direction or direction == 1)
+            and (
+                row['engulfing'] == -100
+                or row['close'] < self.last_open_candle
+            )
+        ):
+            return True
         return False
